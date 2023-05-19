15a055893e17517cd7e31f63beafc28cd901d79e
sonu choure <131000134+choure123@users.noreply.github.com>
2023-05-20 00:46:31 +0530

    ADDED : even number

---
+++ src/EVENORODD.java
@@ -0,0 +1,19 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Scanner;
+
+public class EVENORODD {
+    public static void main(String[] args) {
+        int n;
+        System.out.println("enter any number");
+        Scanner r = new Scanner(System.in);
+        n = r.nextInt();
+        if (n % 2 == 0) ;
+        {
+            System.out.println("even number");
+        }
+    }
+}
+
+
+
