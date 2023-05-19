4e38bdb4d577a0530f7d8b7ca96170787d6a32b0
sonu choure <131000134+choure123@users.noreply.github.com>
2023-05-20 01:03:58 +0530

    ADDED GREATESTNUMBER

---
+++ src/GREATESTNUMBER.java
@@ -0,0 +1,26 @@
+public class GREATESTNUMBER {
+    public static void main(String[] args) {
+        int a=10,b=20,c=30;
+        if(a>b)
+        {
+
+            if (a>c){
+                System.out.println(a);}
+                else
+                System.out.println(b);
+
+            }
+            else {
+                if (b<c){
+            System.out.println(a);}
+        else
+            System.out.println(b);
+
+        }
+
+            }
+
+        }
+
+
+
