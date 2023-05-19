c4b7039f1140d81d603858274027d6055aba7a0e
sonu choure <131000134+choure123@users.noreply.github.com>
2023-05-19 23:38:32 +0530

    ADDED:  repeat until i equals N

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,35 @@ public class Main {
     }
 }
 
+class repeat {
+
+        public static void main(String[] args) {
+            if (args.length == 0) {
+                System.out.println("Please provide a value for N as a command-line argument.");
+                return;
+            }
+
+            int N = Integer.parseInt(args[0]);
+
+            if (N < 0 || N >= 31) {
+                System.out.println("N should be between 0 and 30.");
+                return;
+            }
+
+            int i = 0;
+            while (i != N) {
+                if (i == 31) {
+                    System.out.println("2^" + i + " = Value exceeds integer limit");
+                    break;
+                }
+                int powerOfTwo = 1 << i;
+                System.out.println("2^" + i + " = " + powerOfTwo);
+                i++;
+            }
+        }
+    }
+
+
 
 
 
